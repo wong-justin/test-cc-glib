41871cc50aed2261ca668170df3c865f95360249
wong-justin <jkwongfl@yahoo.com>
2024-06-10 21:16:35 -0400

    add minimal example of cc building glib

---
+++ build.rs
@@ -0,0 +1,21 @@
+fn main() {
+    let out_dir = std::env::var("OUT_DIR").unwrap();
+
+    let source_c_files = std::fs::read_dir("vendor/glib")
+        .unwrap()
+        .filter_map(|entry| {
+            let path = &entry.expect("couldn't unwrap entry inside vendor/").path();
+            let pathstr = path.to_str().expect("couldn't convert vendored .c path to string");
+            match path.is_file() 
+                & path.ends_with(".c") 
+            {
+                true => Some(String::from(pathstr)),
+                false => None
+            }
+        });
+
+    // this step currently fails: `ar` cannot find libglib.a
+    cc::Build::new()
+        .files(source_c_files)
+        .compile("glib");
+}
