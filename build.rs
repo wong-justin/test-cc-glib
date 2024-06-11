02d083bc4ba31f293e4fe584dc04ede134fac15c
wong-justin <jkwongfl@yahoo.com>
2024-06-11 09:59:57 -0400

    fix empty source_c_files
    forgot to walk dir recursively, oops

--- build.rs
+++ build.rs
@@ -1,19 +1,43 @@
 fn main() {
-    let source_c_files = std::fs::read_dir("vendor/glib")
-        .unwrap()
-        .filter_map(|entry| {
-            let path = &entry.expect("couldn't unwrap entry inside vendor/").path();
+    let paths = &mut Vec::<std::path::PathBuf>::new();
+    let source_c_files = read_dir_recursive("vendor/glib", paths);
+
+    std::fs::write("log.txt", format!("{:?}", &source_c_files));
+
+    source_c_files
+        .iter()
+        .filter_map(|path| {
             let pathstr = path.to_str().expect("couldn't convert vendored .c path to string");
-            match path.is_file() 
-                & path.ends_with(".c") 
-            {
+            match (
+                path.ends_with(".c") &
+                !pathstr.contains("tests/") &
+                !pathstr.contains("tools/") &
+                !pathstr.contains("fuzzing/")
+            ) {
                 true => Some(String::from(pathstr)),
                 false => None
             }
         });
 
-    // this step currently fails: `ar` cannot find libglib.a
     cc::Build::new()
         .files(source_c_files)
         .compile("glib");
+}
+
+fn read_dir_recursive<P>(dir: P, accumulated_paths: &mut Vec<std::path::PathBuf>) -> &mut Vec<std::path::PathBuf>
+    where P: AsRef<std::path::Path>
+{
+    let entries = std::fs::read_dir(dir).unwrap();
+    for entry in entries {
+        let path = entry.unwrap().path();
+        match &path.is_dir() {
+            true => {
+                let dup = &mut accumulated_paths.clone();
+                let child_paths = read_dir_recursive(path, dup);
+                accumulated_paths.append(child_paths);
+            },
+            false => accumulated_paths.push(path),
+        };
+    }
+    return accumulated_paths;
 }
